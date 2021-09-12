a02cb2335ce316c76f5eb5da04b6d76f0cb707bc
MạnhG <41892269+manhkhac@users.noreply.github.com>
2021-09-13 06:18:24 +0800

    Update covid.js

--- modules/commands/covid.js
+++ modules/commands/covid.js
@@ -11,7 +11,7 @@ module.exports.config = {
 	}
 }, module.exports.languages = {
 	vi: {
-		return: "====== Thế Giới ======\n😷 Nhiễm: %1\n💚 Đã hồi phục: %2\n💀 Tử vong: %3\n====== Việt Nam ======\n😷 Nhiễm: %4\n😎 Điều trị: %5\n💚 Đã hồi phục: %6\n💀 Tử vong: %7\n\n Thời gian: %8"
+		return: "====== Thế Giới ======\n😷 Nhiễm: %1\n💚 Đã hồi phục: %2\n💀 Tử vong: %3\n====== Việt Nam ======\n😷 Nhiễm: %4\n😎 Điều trị: %5\n💚 Đã hồi phục: %6\n💀 Tử vong: %7\n🤨 Nhiễm 7 ngày: %8\n❤ Hồi phục 7 ngày: %9\n☠️ Tử vong 7 ngày: %10\n\n Thời gian: %11"
 	},
 	en: {
 		return: "====== World ======\n😷 Cases: %1\n😎 Treating: %2\n💚 Recovered: %3\n💀 Deaths: %4\n====== VietNam ======\n😷 Cases: %5\n😎 Treating: %6\n💚 Recovered: %7\n💀 Deaths: %8\n📰 News: %8\nData is updated at: %8 (UTC +7)"
@@ -19,13 +19,12 @@ module.exports.config = {
 }, module.exports.run = async function({
 	api: e,
 	event: n,
-	getText: t
+	getText: a
 }) {
-	const a = global.nodemodule.axios;
-	var i = require("moment-timezone").tz("Asia/Ho_Chi_Minh").format("HH:mm:ss D/MM/YYYY");
-	let o = await a.get("https://static.pipezero.com/covid/data.json");
-	var s = (await o.data).today,
-		r = s.internal || {},
-		d = s.world || {};
-	return e.sendMessage(t("return", d.cases, d.recovered, d.death, r.cases, r.treating, r.recovered, r.death, i), n.threadID, n.messageID)
+	const t = global.nodemodule.axios;
+	let i = await t.get("https://static.pipezero.com/covid/data.json");
+	var o = (await i.data).today,
+		s = (await i.data).overview[6],
+		d = o.world || {};
+	return e.sendMessage(a("return", d.cases, d.recovered, d.death, s.cases, s.treating, s.recovered, s.death, s.avgCases7day, s.avgRecovered7day, s.avgDeath7day, s.date), n.threadID, n.messageID)
 };
