da50d1b970d541c1e2f45a87d3b7517204e9c06c
MạnhG <41892269+manhkhac@users.noreply.github.com>
2021-09-13 05:32:58 +0800

    Update covid.js

--- modules/commands/covid.js
+++ modules/commands/covid.js
@@ -1,6 +1,6 @@
 module.exports.config = {
 	name: "covid",
-	version: "1.0.3",
+	version: "1.0.0",
 	hasPermssion: 0,
 	credits: "ManhG",
 	description: "Lấy thông tin về tình hình dịch bệnh COVID-19",
@@ -11,20 +11,21 @@ module.exports.config = {
 	}
 }, module.exports.languages = {
 	vi: {
-		return: "====== Thế Giới ======\n😷 Nhiễm: %1\n💚 Đã hồi phục: %2\n💀 Tử vong: %3\n====== Việt Nam ======\n😷 Nhiễm: %4\n💚 Đã hồi phục: %5\n💀 Tử vong: %6"
+		return: "====== Thế Giới ======\n😷 Nhiễm: %1\n💚 Đã hồi phục: %2\n💀 Tử vong: %3\n====== Việt Nam ======\n😷 Nhiễm: %4\n😎 Điều trị: %5\n💚 Đã hồi phục: %6\n💀 Tử vong: %7\n\n Thời gian: %8"
 	},
 	en: {
-		return: "====== World ======\n😷 Cases: %1\n💚 Recovered: %2\n💀 Deaths: %3\n====== VietNam ======\n😷 Cases: %4\n💚 Recovered: %5\n💀 Deaths: %6\n📰 News: %7\nData is updated at: %8 (UTC +7)"
+		return: "====== World ======\n😷 Cases: %1\n😎 Treating: %2\n💚 Recovered: %3\n💀 Deaths: %4\n====== VietNam ======\n😷 Cases: %5\n😎 Treating: %6\n💚 Recovered: %7\n💀 Deaths: %8\n📰 News: %8\nData is updated at: %8 (UTC +7)"
 	}
 }, module.exports.run = async function({
 	api: e,
 	event: n,
-	getText: a
+	getText: t
 }) {
-	const o = global.nodemodule.axios;
-	let s = await o.get("https://code.junookyo.xyz/api/ncov-moh/data.json");
-	var t = (await s.data).data,
-		d = t.vietnam || {},
-		i = t.global || {};
-	return e.sendMessage(a("return", i.cases, i.recovered, i.deaths, d.cases, d.recovered, d.deaths), n.threadID, n.messageID)
+	const a = global.nodemodule.axios;
+	var i = require("moment-timezone").tz("Asia/Ho_Chi_Minh").format("HH:mm:ss D/MM/YYYY");
+	let o = await a.get("https://static.pipezero.com/covid/data.json");
+	var s = (await o.data).today,
+		r = s.internal || {},
+		d = s.world || {};
+	return e.sendMessage(t("return", d.cases, d.recovered, d.death, r.cases, r.treating, r.recovered, r.death, i), n.threadID, n.messageID)
 };
