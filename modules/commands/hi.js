f7086413ed0c5c71728d6ab34822b4a95d825528
MạnhG <41892269+manhkhac@users.noreply.github.com>
2021-11-10 06:04:45 +0700

    Create hi.js

---
+++ modules/commands/hi.js
@@ -0,0 +1,87 @@
+module.exports.config = {
+	name: "hi",
+	version: "1.0.1",
+	hasPermssion: 0,
+	credits: "ManhG",
+	description: "",
+	commandCategory: "noprefix",
+	usages: "",
+	cooldowns: 0,
+	denpendencies: {
+		"fs-extra": "",
+		"request": ""
+	}
+};
+
+module.exports.handleEvent = async ({
+	event,
+	api,
+	Users
+}) => {
+	const fs = global.nodemodule["fs-extra"];
+	var {
+		threadID,
+		messageID,
+		body,
+		senderID
+	} = event;
+	const thread = global.data.threadData.get(threadID) || {};
+	if (typeof thread["hi"] !== "undefined" && thread["hi"] == false) return;
+
+	let name = await Users.getNameUser(event.senderID);
+	if (senderID == api.getCurrentUserID()) return;
+
+	function out(data) {
+		api.sendMessage(data, threadID, messageID)
+	}
+	//trả lời
+	var msg = {
+		body: `Chào ${name}, chúc bạn một ngày tốt lành ❤️`,
+		attachment: (await global.nodemodule["axios"]({
+			url: (await global.nodemodule["axios"]('https://apikanna.change-itit.repl.co')).data.data,
+			method: "GET",
+			responseType: "stream"
+		})).data
+	}
+	// Gọi bot
+	var arr = ["hi", "hello", "lô", "hí lô", "chào"];
+	arr.forEach(i => {
+		let str = i[0].toUpperCase() + i.slice(1);
+		if (body === i.toUpperCase() | body === i | str === body) return out(msg)
+	});
+};
+
+module.exports.languages = {
+	"vi": {
+		"on": "Bật",
+		"off": "Tắt",
+		"successText": "hi thành công",
+	},
+	"en": {
+		"on": "on",
+		"off": "off",
+		"successText": "hi success!",
+	}
+}
+
+module.exports.run = async function({
+	api,
+	event,
+	Threads,
+	getText
+}) {
+	const {
+		threadID,
+		messageID
+	} = event;
+	let data = (await Threads.getData(threadID)).data;
+
+	if (typeof data["hi"] == "undefined" || data["hi"] == true) data["hi"] = false;
+	else data["hi"] = true;
+
+	await Threads.setData(threadID, {
+		data
+	});
+	global.data.threadData.set(threadID, data);
+	return api.sendMessage(`${(data["hi"] == false) ? getText("off") : getText("on")} ${getText("successText")}`, threadID, messageID);
+}
