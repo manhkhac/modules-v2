f7a934194b0aacba1c831ff3f1cca3ce8718d4c3
manhkhac <khacmanh.info@gmail.com>
2022-01-13 13:06:43 +0700

    Update sing_mzk.js

--- modules_new_2022/sing_mzk.js
+++ modules_new_2022/sing_mzk.js
@@ -1,84 +1,140 @@
-/**
- * @author MạnhG
- * @warn Do not edit code or edit credits
- */
 module.exports.config = {
     name: "sing",
     version: "1.0.0",
     hasPermssion: 0,
     credits: "MạnhG",
-    description: "Phát video thông qua link YouTube hoặc từ khoá tìm kiếm",
+    description: "Phát nhạc thông qua link YouTube hoặc từ khoá tìm kiếm",
     commandCategory: "Phương tiện",
-    usages: "[searchVideos]",
-    cooldowns: 5,
-    dependencies: {
-        "fs-extra": "",
-        "axios": ""
-    },
+    usages: "[search]",
+    cooldowns: 10,
     envConfig: {
         "API_KEY": "mzkFree_722124509AC10"
     }
 };
 var rdus = Math.floor(Math.random() * 99999999999999);
 var timeSearch = 7; // Tìm kiếm video theo thời gian, ví dụ: 4, 5, 6
-module.exports.run = async function({ event: e, api: t, args: a, body: s }) {
-    const n = require("axios"),
-        { createReadStream: r, statSync: i, writeFileSync: o, createWriteStream: d, unlinkSync: u } = require("fs-extra"),
-        { threadID: c, senderID: l, messageID: g } = e,
-        { API_KEY: h } = global.configModule[this.config.name];
+module.exports.run = async function({
+    event: e,
+    api: t,
+    args: s,
+    body: a
+}) {
+    const r = require("axios"),
+        {
+            createReadStream: n,
+            statSync: i,
+            writeFileSync: o,
+            createWriteStream: d,
+            unlinkSync: u
+        } = require("fs-extra"),
+        {
+            threadID: c,
+            senderID: l,
+            messageID: g
+        } = e,
+        {
+            API_KEY: m
+        } = global.configModule[this.config.name];
     try {
-        if (0 == a.length || !a) return t.sendMessage("» Nhập từ cần tìm!", c, g);
-        if (0 == a.join(" ").indexOf("https://")) {
-            const e = a.join(" ").trim();
+        if (0 == s.length || !s) return t.sendMessage("» Nhập từ cần tìm!", c, g);
+        if (0 == s.join(" ").indexOf("https://")) {
+            const e = s.join(" ").trim();
             try {
-                let { data: a } = await n.get(`https://manhict.tech/video?link=http://youtu.be/${e}&apikey=${h}`);
-                if (a.error) return t.sendMessage(a.error, c);
-                if (t.sendMessage("Đang tải, vui lòng đợi", c, ((e, a) => setTimeout((() => { t.unsendMessage(a.messageID) }), 3e4))), "ok" != a.status) return t.sendMessage("Can't download this video!", c, g);
-                let s = a.title,
-                    d = a.link.medium;
-                var m = __dirname + `/cache/${rdus}.m4a`;
-                const l = (await n.get(d.trim(), { responseType: "arraybuffer" })).data;
-                if (o(m, Buffer.from(l, "utf-8")), i(m).size > 1164e6) return t.sendMessage("File cannot be sent because it is larger than 500MB.", c, (() => u(m)), g);
-                return await t.sendMessage({ body: s, attachment: r(m) }, c, (() => u(m)), g)
-            } catch (e) { return console.log(e), t.sendMessage("Your request could not be processed!", c, g) }
+                let {
+                    data: s
+                } = await r.get(`http://mzkapi.me/video?link=http://youtu.be/${e}&apikey=${m}`);
+                if (s.error) return t.sendMessage(s.error, c);
+                if (t.sendMessage("Đang tải, vui lòng đợi", c, ((e, s) => setTimeout((() => {
+                        t.unsendMessage(s.messageID)
+                    }), 3e4))), "ok" != s.status) return t.sendMessage("Can't download this video!", c, g);
+                let a = s.title,
+                    d = s.link.medium;
+                var h = __dirname + `/cache/${rdus}.m4a`;
+                const l = (await r.get(d.trim(), {
+                    responseType: "arraybuffer"
+                })).data;
+                if (o(h, Buffer.from(l, "utf-8")), i(h).size > 1164e6) return t.sendMessage("File cannot be sent because it is larger than 500MB.", c, (() => u(h)), g);
+                return await t.sendMessage({
+                    body: a,
+                    attachment: n(h)
+                }, c, (() => u(h)), g)
+            } catch (e) {
+                return console.log(e), t.sendMessage("Your request could not be processed!", c, g)
+            }
         } else try {
             var f, y, p = [],
-                b = "",
-                M = 0,
-                v = 0,
-                $ = [];
-            const i = encodeURIComponent(a.join(" "));
-            var { data: I } = await n.get(`https://manhict.tech/youtube?q=${i}&apikey=${h}`);
-            if (I.error) return t.sendMessage(I.error, c);
-            f = I.results;
+                M = "",
+                b = 0;
+            const n = encodeURIComponent(s.join(" "));
+            var {
+                data: v
+            } = await r.get(`http://mzkapi.me/youtube?q=${n}&apikey=${m}`);
+            if (v.error) return t.sendMessage(v.error, c);
+            f = v.results;
             for (let e in f)
                 if (null != f[e].video && (y = f[e].video).duration.length <= timeSearch && "Live" != y.duration) {
-                    let e = __dirname + `/cache/${v+=1}.png`,
-                        t = `${y.thumbnail_src}`,
-                        a = (await n.get(`${t}`, { responseType: "arraybuffer" })).data;
-                    o(e, Buffer.from(a, "utf-8")), $.push(r(__dirname + `/cache/${v}.png`)), M = M += 1, p.push(y.id), b += `${M}.《${y.duration}》 ${y.title}\n\n`
+                    b = b += 1, p.push(y.id), M += `${b}.《${y.duration}》 ${y.title}\n\n`
                 }
-            s = `»🔎 There are ${p.length} results matching your search keyword:\n\n${b}» Please reply (feedback) choose one of the above searches.`;
-            return void t.sendMessage({ attachment: $, body: s }, c, ((t, a) => { client.handleReply.push({ name: this.config.name, messageID: a.messageID, author: e.senderID, idYT: p }) }), g)
-        } catch (e) { return t.sendMessage("The request could not be processed due to an error: " + e.message, c, g) }
-    } catch (e) { return void console.log(e) }
-}, module.exports.handleReply = async function({ event: e, api: t, handleReply: a }) {
-    const s = require("axios"),
-        { createReadStream: n, statSync: r, writeFileSync: i, readdirSync: o, unlinkSync: d } = require("fs-extra"),
-        { threadID: u, senderID: c, messageID: l, body: g } = e,
-        { API_KEY: h } = global.configModule[this.config.name];
-    if (m = g, isNaN(m) || (m < 1 || m > 20)) return t.sendMessage("Choose from 1 -> 20, baby. love uwu ❤️", u, l);
-    var m;
-    t.unsendMessage(a.messageID), t.sendMessage("Đang tải, vui lòng đợi...", u, ((e, a) => setTimeout((() => { t.unsendMessage(a.messageID) }), 3e4)));
+            a = `»🔎 There are ${p.length} results matching your search keyword:\n\n${M}» Please reply (feedback) choose one of the above searches.`;
+            return void t.sendMessage(a, c, ((t, s) => {
+                client.handleReply.push({
+                    name: this.config.name,
+                    messageID: s.messageID,
+                    author: e.senderID,
+                    idYT: p
+                })
+            }), g)
+        } catch (e) {
+            return t.sendMessage("The request could not be processed due to an error: " + e.message, c, g)
+        }
+    } catch (e) {
+        return void console.log(e)
+    }
+}, module.exports.handleReply = async function({
+    event: e,
+    api: t,
+    handleReply: s
+}) {
+    const a = require("axios"),
+        {
+            createReadStream: r,
+            statSync: n,
+            writeFileSync: i,
+            readdirSync: o,
+            unlinkSync: d
+        } = require("fs-extra"),
+        {
+            threadID: u,
+            senderID: c,
+            messageID: l,
+            body: g
+        } = e,
+        {
+            API_KEY: m
+        } = global.configModule[this.config.name];
+    if (h = g, isNaN(h) || (h < 1 || h > 20)) return t.sendMessage("Choose from 1 -> 20, baby. love uwu ❤️", u, l);
+    var h;
+    t.unsendMessage(s.messageID), t.sendMessage("Đang tải, vui lòng đợi...", u, ((e, s) => setTimeout((() => {
+        t.unsendMessage(s.messageID)
+    }), 3e4)));
     try {
-        let { data: o } = await s.get(`https://manhict.tech/singv2/id?id=${a.idYT[e.body-1]}&apikey=${h}`);
+        let {
+            data: o
+        } = await a.get(`http://mzkapi.me/singv2/id?id=${s.idYT[e.body-1]}&apikey=${m}`);
         if (o.error) return t.sendMessage(o.error, u);
         if ("ok" != o.status) return t.sendMessage("Can't download this video!", u, l);
         let c = o.title,
             g = o.link;
         var f = __dirname + `/cache/${rdus}.m4a`;
-        const m = (await s.get(g, { responseType: "arraybuffer" })).data;
-        if (i(f, Buffer.from(m, "utf-8")), r(f).size > 1164e6) return t.sendMessage("File cannot be sent because it is larger than 500MB.", u, (() => d(f)), l);
-        return await t.sendMessage({ body: c, attachment: n(f) }, u, (() => d(f)), l)
-    } catch (e) { return console.log(e), t.sendMessage("Có lỗi xảy ra:" + e, u, l) }
+        const h = (await a.get(g, {
+            responseType: "arraybuffer"
+        })).data;
+        if (i(f, Buffer.from(h, "utf-8")), n(f).size > 1164e6) return t.sendMessage("File cannot be sent because it is larger than 500MB.", u, (() => d(f)), l);
+        return await t.sendMessage({
+            body: c,
+            attachment: r(f)
+        }, u, (() => d(f)), l)
+    } catch (e) {
+        return console.log(e), t.sendMessage("Có lỗi xảy ra:" + e, u, l)
+    }
 };
